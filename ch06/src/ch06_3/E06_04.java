cd83be3b86fa7c9851b066537605571b69b57e5f
Luverduck <hyekko92@gmail.com>
2023-04-18 22:10:34 +0900

    실습 6-4. 단순 선택 정렬

--- ch06/src/ch06_3/E06_04.java
+++ ch06/src/ch06_3/E06_04.java
@@ -3,17 +3,9 @@ package ch06_3;
 import java.util.Scanner;
 
 public class E06_04 {
-
-	// 배열의 idx1 번째 원소와 idx2 번째 원소 교환
-	static void swap(int[] array, int idx1, int idx2) {
-		int temp = array[idx2];
-		array[idx2] = array[idx1];
-		array[idx1] = temp;
-	}
 	
 	// 단순 선택 정렬
 	static void selectionSort(int[] array) {
-		int count = 0;
 		for(int i = 0 ; i < array.length ; i ++) {
 			int min = i;		// 현재 루프에서 최소값의 index
 			for(int j = i ; j < array.length ; j ++) {
@@ -21,10 +13,11 @@ public class E06_04 {
 					min = j;
 				}
 			}
-			swap(array, i, min);
-			count ++;
+			// 교환
+			int temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
 		}
-		System.out.println("총 교환 수 : " + count);
 	}
 	
 	// 정렬
